f0198db4de4d5fbf395f3a363e33b6fa4fbdea9d
jackavryashnik <jackavryashnik@gmail.com>
2024-04-03 18:23:29 +0300

    update App and Contacts page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,12 @@ import NotFoundPage from '../../pages/NotFoundPage';
 import Layout from '../Layout/Layout';
 import PrivateRout from '../PrivateRoute/PrivateRoute';
 import RestrictedRoute from '../RestrictedRoute/RestrictedRoute';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from '../../redux/auth/operations';
 import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import { selectIsRefreshing } from '../../redux/auth/selectors';
 
 const HomePage = lazy(() => import('../../pages/HomePage'));
 const Login = lazy(() => import('../../pages/Login'));
@@ -16,6 +17,7 @@ const Contacts = lazy(() => import('../../pages/Contacts'));
 
 function App() {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -24,6 +26,9 @@ function App() {
   return (
     <>
       <Layout>
+        {
+        isRefreshing ?
+        <p>Loading...</p> :
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
@@ -42,6 +47,7 @@ function App() {
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Suspense>
+        }
       </Layout>
     </>
   );
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import ContactForm from '../components/ContactForm/ContactForm';
 import ContactList from '../components/ContactList/ContactList';
 import SearchBox from '../components/SearchBox/SearchBox';
 import { selectError, selectLoading } from '../redux/contacts/selectors';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/contacts/operations';
 
 const Contacts = () => {
+  const dispatch = useDispatch();
   const error = useSelector(selectError);
   const loading = useSelector(selectLoading);
+  
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch]);
 
   return <div>
   <h1>Phonebook</h1>
